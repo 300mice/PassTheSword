6b193723d62acd4e0b441198add2d4a781312473
BenAnderson <31300554+300mice@users.noreply.github.com>
2025-01-21 14:45:14 +1100

    Figuring out AI :)

---
+++ Assets/Scripts/AIBrain.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using UnityEditor;
+
+// Defines logic for the behaviour of Party Members
+public class AIBrain : MonoBehaviour
+{
+
+    [TagSelector] 
+    public string[] TargetPriorities;
+    
+    private GameObject target;
+    private NavMeshAgent agent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(BrainLoop(1.0f));
+    }
+
+    private GameObject GetTarget()
+    {
+        for (int i = 0; i < TargetPriorities.Length; i++)
+        {
+            GameObject enemy = GameObject.FindGameObjectWithTag(TargetPriorities[i]);
+            if (enemy != null)
+            {
+                return enemy.gameObject;
+            }
+        }
+
+        return null;
+    }
+
+    IEnumerator BrainLoop(float waitTime)
+    {
+        if (target == null)
+        {
+            target = GetTarget();
+        }
+        agent.destination = target.transform.position;
+        
+        yield return new WaitForSeconds(waitTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
