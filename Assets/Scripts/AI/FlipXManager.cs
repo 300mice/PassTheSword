e65b36b269d411bccc45d9add986857fdb81ff8d
300mice <ben@sacada.net>
2025-01-24 19:33:50 +1100

    Update FlipXManager.cs

--- Assets/Scripts/AI/FlipXManager.cs
+++ Assets/Scripts/AI/FlipXManager.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,23 +12,43 @@ public class FlipXManager : MonoBehaviour
 
 
     // used to track facing for sprite and sword positions
-    private int currentFacing = -1;
+    private int currentFacing = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         brain = GetComponent<AIBrain>();
         material = sprite.material;
-        material.SetInt("_IsFacingRight?", currentFacing);
+        /*material.EnableKeyword("_NORMALMAP");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.EnableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_EMISSION");
+        material.EnableKeyword("_PARALLAXMAP");
+        material.EnableKeyword("_DETAIL_MULX2");
+        material.EnableKeyword("_METALLICGLOSSMAP");
+        material.EnableKeyword("_SPECGLOSSMAP");*/
+        
+        
+        
+        
+        sprite.material.SetFloat("_IsFacingLeft", currentFacing);
+        //int asd123 = material.GetInteger("_IsFacingLeft");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         // check facing
-        if (Mathf.Sign(brain.ReturnTarget().transform.position.x - transform.position.x) != currentFacing)
+        if (!brain.CurrentAction.Target)
         {
-            currentFacing = (int)Mathf.Sign(brain.ReturnTarget().transform.position.x - transform.position.x);
+            return;
+        }
+        
+        if (Mathf.Sign(brain.CurrentAction.Target.transform.position.x - transform.position.x) != currentFacing)
+        {
+            currentFacing = (int)Mathf.Sign(brain.CurrentAction.Target.transform.position.x - transform.position.x);
             FlipX();
         }
 
@@ -37,8 +58,8 @@ public class FlipXManager : MonoBehaviour
     void FlipX()
     {
 
-        swordPos.transform.localScale = new Vector3(-currentFacing, 1, 1);
-
-        material.SetInt("_IsFacingRight?", currentFacing);
+        //swordPos.transform.localScale = new Vector3(-currentFacing, 1, 1);
+        sprite.material.SetFloat("_IsFacingLeft", currentFacing > 0? 0 : 1);
+        sprite.flipX = currentFacing < 0;
     }
 }
