8824ca996bcb67c5a6e2c4cf0915f4b1ddb560cb
300mice <ben@sacada.net>
2025-01-31 09:40:35 +1100

    healthbar flashes

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,15 @@ public class HealthBar : MonoBehaviour
     public Color healthFillColor;
 
     public GameObject targetObject;
+
+    private bool bFlashing;
+
+    private Material _material;
+
+    private Color originalColor;
+    private Renderer objectRenderer;
+    private MaterialPropertyBlock propertyBlock;
+    private float oldHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -15,21 +25,51 @@ public class HealthBar : MonoBehaviour
             healthComponent = targetObject.GetComponent<HealthComponent>();
         else
             healthComponent = GetComponentInParent<HealthComponent>();
+        
+        objectRenderer = healthFill.GetComponent<Renderer>();
+        propertyBlock = new MaterialPropertyBlock();
     }
     
     void Start()
     {
         healthComponent.OnHealthChanged.AddListener(OnHealthChanged);
-        healthFill.color = healthFillColor;
+        _material = new Material(healthFill.material);
+        healthFill.material = _material;
+        originalColor = _material.GetColor("_Colour");
+        oldHealth = healthComponent.health;
     }
 
     void OnHealthChanged()
     {
         healthFill.fillAmount = healthComponent.health / healthComponent.GetMaxHealth();
+        
+        if (!bFlashing)
+        {
+            bFlashing = true;
+            if (healthComponent.health - oldHealth > 0)
+            {
+                StartCoroutine(HealthbarFlash(Color.green));
+            }
+            else
+            {
+                StartCoroutine(HealthbarFlash(Color.white));
+            }
+            
+        }
+        oldHealth = healthComponent.health;
+        
     }
 
     public void SetHealth(HealthComponent h)
     {
         healthComponent = h;
+    }
+
+    IEnumerator HealthbarFlash(Color newColor)
+    {
+        _material.SetColor("_Colour",newColor);
+        yield return new WaitForSeconds(0.1f);
+        _material.SetColor("_Colour", originalColor);
+        bFlashing = false;
     }
 }
